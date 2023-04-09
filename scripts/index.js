fba741dbaf10a37255d8ae61a6779a09ad8b346d
sukriti singh <sukriti.singh899@gmail.com>
2023-04-10 01:00:08 +0530

    Initial commit

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+const header = document.getElementById("navigation_bar");
+function openNav() {
+  document.getElementById("mySidenav").style.width = "329px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
