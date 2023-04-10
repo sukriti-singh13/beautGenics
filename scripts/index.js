487f24b805f9b0623f7325aecc48ea784a986b05
sukriti singh <sukriti.singh899@gmail.com>
2023-04-10 15:35:45 +0530

    added icons

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,3 @@
-const header = document.getElementById("navigation_bar");
 function openNav() {
   document.getElementById("mySidenav").style.width = "329px";
 }
@@ -6,3 +5,18 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+function openCity(evt, cityName) {
+  let i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+document.getElementById("defaultOpen").click();
